791b48f9f2c3eba47ff5f4acad851d50cd8fd56e
Kouhei Sutou <kou@clear-code.com>
2013-11-14 22:50:56 +0900

    Add missing initializations

--- hello.c
+++ hello.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <libintl.h>
+#include <locale.h>
 
 int
 main(void)
 {
+  setlocale(LC_ALL, "");
+  bindtextdomain("hello", "locale");
+  textdomain("hello");
   puts(gettext("Hello"));
   return 0;
 }
